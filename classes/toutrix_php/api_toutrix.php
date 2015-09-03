a7933189be8b2dbbe6dfb500ca840c800017e6fc
c3rv3au <c3rv3au@gmail.com>
2015-09-03 16:47:58 -0400

    Initial commit

---
+++ classes/toutrix_php/api_toutrix.php
@@ -0,0 +1,165 @@
+<?
+define("channel_mainstream",1);
+define("channel_adult",2);
+class api_toutrix_adserver extends api_toutrix {
+  // TODO - Change to real server, and to https
+  var $endpoint = "http://serv.toutrix.com/api";
+  var $p_login_user = "/users/login";
+  var $p_user = "/users";
+  var $p_channels = "/channels";
+  var $p_adtypes = "/adtypes";
+  var $p_campaign = "/users/:userId/campaigns";
+  var $p_creative = "/users/:userId/creatives";
+  var $p_sites = "/users/:userId/sites";
+  var $p_zones = "/sites/:siteId/zones";
+  var $p_flight_update = "/flights/:id";
+  var $p_flight = "/campaigns/:campaignId/flights";
+  var $p_creative_flight = "/creatives_flight";
+  var $p_target = "/targetings";
+  function login($username, $password) {
+     $datas = array('path'=> $this->p_login_user,
+                    'method'=> 'POST',
+                    'fields'=> array('username'=>$username, 'password'=>$password) );
+     $output = $this->launch_request($datas);
+     if (strlen($output)==0) {
+       echo "Server not reachable\n";
+       return false;
+     }
+     $returns = json_decode($output,false);
+     if ($returns->error != null) {
+       echo "ERROR\n";
+       return false;
+     }
+     $this->access_token = $returns->id;
+     $this->userId = $returns->userId;
+     echo "Access token is: " . $this->access_token . "\n";
+     if (strlen($this->access_token)>0) {
+       return true;
+     } else {
+       return false;
+     }
+  }
+  function user_create($fields) {
+     $path = $this->do_path($this->p_user, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function setAccessToken($token) {
+    $this->access_token = $token;
+    // TODO - We have to get the userId
+  }
+  function flight_update($fields) {
+     $path = $this->do_path($this->p_flight_update, $fields);
+     $datas = array('path'=> $path,
+                    'method'=> 'PUT',
+                    'fields'=> $fields
+                   );
+     $output = $this->launch_request($datas);
+     $returns = json_decode($output,false);
+     return $returns;
+  }
+  function model_create($path,$fields) {
+     $datas = array('path'=> $path,
+                    'method'=> 'POST',
+                    'fields'=> $fields
+                   );
+     $output = $this->launch_request($datas);
+     return json_decode($output,false);
+  }
+  function model_get($path,$fields) {
+     $datas = array('path'=> $path,
+                    'method'=> 'GET',
+                    'fields'=> $fields
+                   );
+     $output = $this->launch_request($datas);
+     return json_decode($output,false);
+  }
+  function channels_get($fields) {
+     $path = $this->do_path($this->p_channels, $fields);
+     return $this->model_get($path, $fields);
+  }
+  function adtypes_get($fields) {
+     $path = $this->do_path($this->p_adtypes, $fields);
+     return $this->model_get($path, $fields);
+  }
+  function campaign_create($fields) {
+     $path = $this->do_path($this->p_campaign, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function creative_create($fields) {
+     $path = $this->do_path($this->p_creative, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function site_create($fields) {
+     $path = $this->do_path($this->p_sites, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function zone_create($fields) {
+     $path = $this->do_path($this->p_zones, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function flight_create($fields) {
+     $path = $this->do_path($this->p_flight, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function creative_flight_create($fields) {
+     $path = $this->do_path($this->p_creative_flight, $fields);
+     return $this->model_create($path, $fields);
+  }
+  function target_create($fields) {
+     $path = $this->do_path($this->p_target, $fields);
+     return $this->model_create($path, $fields);
+  }
+}
+class api_toutrix {
+  var $ch;
+  var $last_code;
+  var $access_token;
+  var $userId;
+  function __construct() {
+    $this->ch = curl_init();
+    $arr = array();
+    curl_setopt ($this->ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt ($this->ch, CURLOPT_SSL_VERIFYPEER, 0); 
+    curl_setopt ($this->ch, CURLOPT_RETURNTRANSFER, 1); 
+    $headers = array(
+      'Accept: application/json',
+      'Content-Type: application/json',
+    );
+    curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
+  }
+  function do_path($path, $fields) {
+    $result = $path;
+    if (!empty($fields->id))
+      $result = str_replace(':id', $fields->id, $result);
+    if (!empty($fields->campaignId))
+      $result = str_replace(':campaignId', $fields->campaignId, $result);
+    if (!empty($this->userId))
+      $result = str_replace(':userId', $this->userId, $result);
+    if (!empty($fields->siteId))
+      $result = str_replace(':siteId', $fields->siteId, $result);
+    if (!empty($fields->zoneId))
+      $result = str_replace(':zoneId', $fields->zoneId, $result);
+    return $result;
+  }
+  function launch_request($datas) {
+    $url = $this->endpoint . $datas['path'];
+//echo "URL : " . $url . "\n";
+    if (strlen($this->access_token)>0)
+      $url .= "?access_token=" . $this->access_token;
+    $fields = json_encode($datas['fields']);
+//echo "Fields: " . $fields . "\n";
+//echo "Methode used: " . $datas['method'] . "\n";
+    curl_setopt($this->ch, CURLOPT_URL, $url); 
+    if ($datas['method'] == 'POST' || $datas['method'] == 'PUT') {
+      curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $datas['method']);   
+      curl_setopt($this->ch, CURLOPT_POSTFIELDS, $fields);
+      curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($fields))                                                                       
+      );
+    }
+    $output = curl_exec($this->ch); 
+    $this->last_code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+    return $output;
+  }
+}
---
+++ wp_toutrix.php
@@ -0,0 +1,359 @@
+<?php
+/*
+Plugin Name: TouTrix AdServer
+Plugin URI:  http://toutrix.com/wp_toutrix
+Description: This plugin connect to TouTrix AdMedia Server, create zone to earn money to show ads. You casn also ask a withdrawal without leaving your website.
+Version:     0.1
+Author:      TouTrix
+Author URI:  http://toutrix.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: toutrix-adserver
+*/
+
+//	defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+require_once "classes/toutrix_php/api_toutrix.php";
+
+
+add_option( 'ad_toutrix_username', '', '', 'yes' );
+add_option( 'ad_toutrix_password', '', '', 'yes' );
+add_option( 'ad_toutrix_access_token', '', '', 'yes' );
+add_option( 'ad_toutrix_website_id', '', '', 'yes' );
+add_option( 'ad_toutrix_zone_id', '', '', 'yes' );
+
+// Hook for adding admin menus
+add_action('admin_menu', 'mt_add_pages');
+
+//add_action( 'widgets_init', 'register_my_widget' );
+
+global $adserver;
+$adserver = new api_toutrix_adserver();
+
+global $zoneId;
+
+// action function for above hook
+function mt_add_pages() {
+    // Add a new submenu under Settings:
+    //add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
+
+    // Add a new submenu under Tools:
+    //add_management_page( __('Test Tools','menu-test'), __('Test Tools','menu-test'), 'manage_options', 'testtools', 'mt_tools_page');
+
+    // Add a new top-level menu (ill-advised):
+    add_menu_page(__('TouTrix','menu-toutrix'), __('TouTrix','menu-toutrix'	), 'manage_options', 'mt_toutrix_page-handle', 'mt_toutrix_page' );
+
+    // Add a submenu to the custom top-level menu:
+    add_submenu_page('mt_toutrix_page-handle', __('Stats','menu-stats'), __('Stats','menu-stats'), 'manage_options', 'sub-page', 'mt_stats_page');
+}
+
+function get_channel() {
+}
+
+function toutrix_get_token() {
+    global $adserver;
+
+    $toutrix_username = get_option("ad_toutrix_username");
+    $toutrix_password  = get_option("ad_toutrix_password");
+    $toutrix_access_token  = get_option("ad_toutrix_access_token");
+
+    if (strlen($toutrix_username)>0 && strlen($toutrix_password)>0) {
+        // Si nous n'avons pas d'access token, ca nous en prend un
+        //if (strlen($toutrix_access_token)==0) {
+           if ($adserver->login($toutrix_username, $toutrix_password)) {
+             //echo "Access Token is now: " . $adserver->access_token . "<br/>";
+             update_option( "ad_toutrix_access_token", $adserver->access_token );
+             return true;
+           } else {
+?>
+<div class="updated"><p><strong><?php _e('Cant connect with these credentials.', 'menu-test' ); ?></strong></p></div>
+<?
+             update_option( "ad_toutrix_access_token", '' );
+           }
+        //}
+    }
+    return false;
+}
+
+// mt_toplevel_page() displays the page content for the custom Test Toplevel menu
+function mt_toutrix_page() {
+    global $adserver;
+
+    echo "<h2>AdMedia configuration</h2>";
+
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+    // Read in existing option value from database
+    $toutrix_username = get_option("ad_toutrix_username");
+    $toutrix_password  = get_option("ad_toutrix_password");
+    $toutrix_access_token  = get_option("ad_toutrix_access_token");
+    $toutrix_website_id  = get_option("ad_toutrix_website_id");
+    $toutrix_zone_id  = get_option("ad_toutrix_zone_id");
+
+    if( isset($_POST[ "signup" ]) && $_POST[ "signup" ] == 'Y' ) {
+        update_option( "ad_toutrix_access_token", "" );
+        update_option( "ad_toutrix_website_id", "" );
+        update_option( "ad_toutrix_zone_id", "" );
+        update_option( "ad_channel_id", $_POST['channelId'] );
+
+        // Read their posted value
+        $toutrix_username = $_POST[ "ad_toutrix_username" ];
+        $toutrix_password = $_POST[ "ad_toutrix_password" ];
+        $user = new stdClass();
+        $user->username = $_POST[ "ad_toutrix_username" ];
+        $user->password = $_POST[ "ad_toutrix_password" ];
+        $user->email = $_POST[ "ad_toutrix_email" ];
+        $user->refererId = $_POST[ "refererId" ];
+//var_dump($adserver);
+        $user = $adserver->user_create($user);
+        if ($user->error && !$user->error->message == "path is not defined") {
+?>
+<div class="updated"><p><strong><?php _e($user->error->message, 'menu-test' ); ?></strong></p></div>
+<?
+        } else {
+          update_option( "ad_toutrix_username", $toutrix_username  );
+          update_option( "ad_toutrix_password", $toutrix_password  );
+?>
+<div class="updated"><p><strong><?php _e('TouTrix account is created.', 'menu-test' ); ?></strong></p></div>
+<?php
+        }
+    }
+
+    if( isset($_POST[ "config" ]) && $_POST[ "config" ] == 'Y' ) {
+        // Read their posted value
+        $toutrix_username = $_POST[ "ad_toutrix_username" ];
+        $toutrix_password = $_POST[ "ad_toutrix_password" ];
+
+        // Save the posted value in the database
+        update_option( "ad_toutrix_username", $toutrix_username  );
+        update_option( "ad_toutrix_password", $toutrix_password  );
+
+        // Put a "settings saved" message on the screen
+
+?>
+<div class="updated"><p><strong><?php _e('settings saved.', 'menu-test' ); ?></strong></p></div>
+<?php
+    }
+	
+  if (strlen($toutrix_username)==0 && strlen($toutrix_password)==0) {
+     $channels = $adserver->channels_get(array());
+     //var_dump($channels);	
+     echo '<div class="wrap">';
+?>
+
+<h1>Signup on TouTrix</h2>
+<form name="form1" method="post" action="">
+<input type="hidden" name="signup" value="Y">
+<input type="hidden" name="refererId" value="1">
+
+<p><?php _e("Username:", 'menu-test' ); ?> 
+<input type="text" name="ad_toutrix_username" value="<?php echo $toutrix_username; ?>" size="20">
+</p>
+
+<p><?php _e("Password:", 'menu-test' ); ?> 
+<input type="text" name="ad_toutrix_password" value="<?php echo $toutrix_password; ?>" size="20">
+</p>
+
+<p><?php _e("Channel:", 'menu-test' ); ?> 
+<select name='channelId'>
+<? foreach ($channels as $channel) { ?>
+<option name='<? echo $channel->id; ?>'><? echo $channel->Title; ?></option>
+<? } ?>
+</select> Choose the good channel for your website. We may change it for you later.
+
+</p>
+
+<p><?php _e("Your email:", 'menu-test' ); ?> 
+<input type="text" name="ad_toutrix_email" value="<?php echo $toutrix_email; ?>" size="20">
+</p>
+
+<hr />
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php _e("Signup on TouTrix", 'menu-test'); ?>" />
+</p>
+
+</form>
+</div>
+
+<? } elseif (strlen($toutrix_username)>0 && strlen($toutrix_password)>0) {
+       //echo "Getting token<br/>";
+       if (toutrix_get_token()) {
+          //echo "Got Token<br/>";
+          $toutrix_access_token  = get_option("ad_toutrix_access_token");
+          $toutrix_website_id  = get_option("ad_toutrix_website_id");
+          if (strlen($toutrix_website_id)==0) {
+              echo "Creating website...<br/>";
+              $toutrix_channel_id  = get_option("ad_channel_id");
+
+              $site = new stdClass();
+              $site->Title = get_bloginfo();;
+              $site->Url = get_site_url();
+              $site->Description = get_bloginfo ( 'description' );
+              $site->channelId = $toutrix_channel_id;
+              $site = $adserver->site_create($site);
+              //echo "Create website: ";
+              //var_dump($site);
+              if ($site->id > 0) {
+                update_option( "ad_toutrix_website_id", $site->id);
+                $toutrix_website_id = $site->id;
+              } else {
+                echo "<font color='red'>Can't find WebSite ID</font>";
+              }
+              echo "<br/>";
+          }
+          if (strlen($toutrix_zone_id)==0 && strlen($toutrix_website_id)>0) {
+              //echo "Creating zone...<br/>";
+              $zone = new stdClass();
+              $zone->Name = get_bloginfo();;
+              $zone->siteId = $toutrix_website_id;
+              $zone->channelId = channel_mainstream;
+
+              $zone = $adserver->zone_create($zone);
+              //echo "Create zone: ";
+              //var_dump($zone);
+              if ($zone->id > 0) {
+                update_option( "ad_toutrix_zone_id", $zone->id);
+                $toutrix_zone_id = $zone->id;
+              } else {
+                echo "<font color='red'>Can't find Zone ID</font>";
+              }
+              echo "<br/>";
+          }
+
+       }
+    }
+    
+
+    // Now display the settings editing screen
+?>
+<h1>Connect with your TouTrix account</h2>
+<?
+    echo '<div class="wrap">';
+    ?>
+<form name="form1" method="post" action="">
+<input type="hidden" name="config" value="Y">
+
+<p><?php _e("TouTrix Username:", 'menu-test' ); ?> 
+<input type="text" name="ad_toutrix_username"" value="<?php echo $toutrix_username; ?>" size="20">
+</p>
+
+<p><?php _e("TouTrix password:", 'menu-test' ); ?> 
+<input type="text" name="ad_toutrix_password" value="<?php echo $toutrix_password; ?>" size="20">
+</p>
+
+<p><?php _e("Access Token:", 'menu-test' ); ?> 
+<?php echo $toutrix_access_token; ?>
+</p>
+
+<p><?php _e("Website ID:", 'menu-test' ); ?> 
+<?php echo $toutrix_website_id; ?>
+</p>
+
+<p><?php _e("Default Zone ID:", 'menu-test' ); ?> 
+<?php echo $toutrix_zone_id; ?>
+</p>
+
+<hr />
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+</div>
+
+
+<?php
+}
+
+// mt_sublevel_page() displays the page content for the first submenu
+// of the custom Test Toplevel menu
+function mt_stats_page() {
+    echo "<h2>Stats</h2>";
+}
+
+add_action('widgets_init', 'toutrix_init');
+
+function toutrix_init() {
+  register_widget('TouTrix_Widget');
+}
+
+// Creating the widget 
+class TouTrix_Widget extends WP_Widget {
+
+function __construct() {
+  parent::__construct(
+  // Base ID of your widget
+  'TouTrix_Widget', 
+
+  // Widget name will appear in UI
+  __('TouTrix Widget', 'TouTrix_Widget_domain'), 
+
+  // Widget description
+  array( 'description' => __( 'Show an ad', 'TouTrix_Widget_domain' ), ) 
+  );
+}
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+      $toutrix_zone_id  = get_option("ad_toutrix_zone_id");
+  $title = apply_filters( 'widget_title', $instance['title'] );
+  // before and after widget arguments are defined by themes
+  echo $args['before_widget'];
+  if ( ! empty( $title ) )
+  echo $args['before_title'] . $title . $args['after_title'];
+
+  echo "<script src='http://serv.toutrix.com/js/creative?zone_id=" . $toutrix_zone_id . "&adtypeId=2'></script>";
+
+  echo $args['after_widget'];
+}
+		
+// Widget Backend 
+public function form( $instance ) {
+  global $adserver;
+  $adtypes = $adserver->adtypes_get(array());
+  //var_dump($channels);	
+
+  if ( isset( $instance[ 'title' ] ) ) {
+    $title = $instance[ 'title' ];
+    $adtypeId = $instance[ 'adtypeId' ];
+  }
+  else {
+    $title = __( 'Ads', 'TouTrix_Widget_domain' );
+  }
+  // Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+<select name='<? echo $this->get_field_name( 'adtypeId' );?>'>
+<? foreach ($adtypes as $adtype) { ?>
+<option value='<? echo $adtype->id; ?>'<? if ($adtypeId == $adtype->id) echo " selected"; ?>><? echo $adtype->name; ?></option>
+<? } ?>
+</select>
+</p>
+<?php 
+}
+	
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+  $instance = array();
+  $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+  $instance['adtypeId'] = ( ! empty( $new_instance['adtypeId'] ) ) ? strip_tags( $new_instance['adtypeId'] ) : '';
+  return $instance;
+}
+
+} // Class TouTrix_Widget ends here
+
+// Register and load the widget
+function wpb_load_widget() {
+	register_widget( 'TouTrix_Widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
